c81af54b33a1d6ff2f26745b19628f87060a256b
v.burjan <1030466@stud.hs-mannheim.de>
2016-04-22 12:39:48 +0200

    using arraylist and vector to get the position of touchpoints

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Collections;
+using WpfApplication4;
 
 namespace WpfTouchFrameSample
 {
@@ -8,6 +10,7 @@ namespace WpfTouchFrameSample
     {
         private int touches = 0;
         double[,] position = new double[8,2];
+        ArrayList vectorList = new ArrayList();
       
         public MainWindow()
         {
@@ -27,15 +30,40 @@ namespace WpfTouchFrameSample
             el.CaptureTouch(e.TouchDevice);
             _touchPoint = e.TouchDevice.GetTouchPoint(this.canvas1);
 
+            touches++;
+
+            
+            vectorList.Add(new Vector(_touchPoint.Position.X, _touchPoint.Position.Y));
+            /*
             position[touches, 0] = _touchPoint.Position.X;
             position[touches, 1] = _touchPoint.Position.Y;
             touches++;
+            */
+            calculator(position);
         
             Nummer.Dispatcher.Invoke(new UpdateTextCallback(this.UpdateText), touches.ToString());
 
             xy.AppendText(touches.ToString() + " X " + _touchPoint.Position.X.ToString() + " Y " + _touchPoint.Position.Y.ToString() +"\n");
 
             e.Handled = true;
+        }
+
+        void calculator(double[,] position )
+        {
+            int ix=0;
+            int iy=0;
+            int ret=0;
+            Array dis = new double[8,2];
+
+            double[,] leer; 
+            if ( position[1, 0] == null) return;
+
+            while ( ret <=8 )
+            {
+                dis = (position[ix] - position[ix++]);
+            }
+
+
         }
 
         void OnTouchUp(object sender, TouchEventArgs e)
@@ -61,6 +89,7 @@ namespace WpfTouchFrameSample
 
         private void UpdateText(string message)
         {
+            // hier evtl. den calculator aufrufen
             Nummer.Document.Blocks.Clear();
             Nummer.AppendText(message);
            // xy.Document.Blocks.Clear();
